5a8a97d4208f329f80866f128982f460751d7ceb
KingofKonigsberg <itzlegendyolo@gmail.com>
2026-02-24 17:46:45 +0200

    dz 10.11.2025

--- Task-23/11671.py
+++ Task-23/11671.py
@@ -1,1 +1,25 @@
-def f(start,end):
+def f(start, cnt=0):
+    if cnt == 15: return {start}
+    return f(start + 10, cnt + 1) | f(start - 5, cnt + 1)
+
+print(len(f(1)))
+
+######################################
+
+def f(cur, cnt=0):
+    if cnt == 15:
+        spots.add(cur)
+        return
+    f(cur + 10, cnt + 1)
+    f(cur - 5, cnt + 1)
+
+spots = set()
+f(1)
+print(len(spots))
+
+######################################
+
+dots = {1}
+for i in range(15):
+    dots = {x + 10 for x in dots} | {x - 5 for x in dots}
+print(len(dots))
--- Task-23/2341.py
+++ Task-23/2341.py
@@ -1,11 +1,24 @@
-d=set()
-def f(start,cnt):
-    if cnt==8:
-        if 1000<=start<=1024:
-            d.add(start)
-    else:
-        f(start+1,cnt+1)
-        f(start + 5, cnt + 1)
-        f(start * 3, cnt + 1)
-f(1,0)
-print(len(d))
+
+def f(start, k):
+    if k == 8:
+        if start in range(1000, 1025): return {start}
+        return set()
+    return f(start + 1, k + 1) | f(start + 5, k + 1) | f(start * 3, k + 1)
+
+print(len(f(1, 0)))
+
+#######################################
+
+def f(cur, cnt=0):
+    if cnt == 8:
+        if 1000 <= cur <= 1024:
+            spots.add(cur)
+        return
+    f(cur + 1, cnt + 1)
+    f(cur + 5, cnt + 1)
+    f(cur * 3, cnt + 1)
+
+
+spots = set()
+f(1)
+print(len(spots))
