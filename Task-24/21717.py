d2f42bc67c56102df24fcfd2711924a1a90ce0b9
ItzLeg <itzlegendyolo@gmail.com>
2026-05-15 17:35:16 +0200

    .gitignore

---
+++ Task-24/21717.py
@@ -0,0 +1,14 @@
+
+with open('./files/24_21717.txt') as file:
+    data=file.readline()
+data=data.split('RSQ')
+
+ans=10**20
+for i in range(1,len(data)-129-1):
+    line='RSQ'+'RSQ'.join(data[i:i+129])+'RSQ'
+    dop=data[i+129]+'RSQ'
+    for j in 'RSFGW':
+        dop=dop.replace(j,'*')
+    line+=dop[:dop.find('*')+1]
+    ans=min(ans,len(line))
+print(ans)
