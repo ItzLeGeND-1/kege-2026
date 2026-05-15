6e8891c2d7fda3cb9ced3bfc3d3a8f4e4bac3b9c
ItzLeg <itzlegendyolo@gmail.com>
2026-05-15 16:50:54 +0200

    .gitignore

---
+++ Task-24/16388-regular.py
@@ -0,0 +1,6 @@
+from re import finditer
+with open('./files/24_16388.txt') as file:
+    data=file.readline()
+pattern=r'(LMN|MN|N)?(KLMN)+(KLM|KL|K)?'
+matches=[match.group() for match in finditer(pattern,data)]
+print(len(max(matches,key=len)))
---
+++ Task-24/16388-zamena.py
@@ -0,0 +1,17 @@
+with open('./files/24_16388.txt') as file:
+    data=file.readline()
+
+data=data.replace('KLMN','****')
+data=data.replace('LMN*',' ****')
+data=data.replace('MN*',' ***')
+data=data.replace('N*',' **')
+data=data.replace('*KLM','**** ')
+data=data.replace('*KL','*** ')
+data=data.replace('*K','** ')
+
+for i in 'KLMN':
+    data=data.replace(i,' ')
+
+data=data.split()
+
+print(len(max(data,key=len)))
